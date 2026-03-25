50686814644c68f8e85334db0b28312a877e8415
DRJ <revdan@users.noreply.github.com>
2026-03-25 14:48:27 +0000

    Add dialyxir to deps and CI (#3)
    Adds dialyxir for static type checking with PLT caching in GitHub
    Actions.

--- mix.exs
+++ mix.exs
@@ -6,11 +6,17 @@ defmodule Wallop.MixProject do
       apps_path: "apps",
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/project.plt"},
+        plt_add_apps: [:mix]
+      ]
     ]
   end
 
   defp deps do
-    []
+    [
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+    ]
   end
 end
